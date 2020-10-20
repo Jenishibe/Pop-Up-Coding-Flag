c3eaf6eedca48b14cf93d1e7502cd91c0cc20635
Jenishibe <tuo32764@temple.edu>
2020-10-20 16:07:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pop-up-w3schools
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pop-up-w3schools)
---
+++ script.js
@@ -0,0 +1,4 @@
+console.log('hello!')
+function togglePopup(){
+  document.getElementById("popup-1").classList.toggle("active");
+}
